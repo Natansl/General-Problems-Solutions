203360a1fae6e30efbc5ee1c226960995ae2ae85
Natan Siller Laurett <natan_siller@hotmail.com>
2021-01-26 13:03:47 -0300

    Greedy Solution to Fractional KSP added

--- knapsackProblem.py
+++ knapsackProblem.py
@@ -43,8 +43,27 @@ def kspDynProg(items, capacity):
                 m[i+1][j] = max([m[i][j], m[i][j - items[i][1]] + items[i][0]])
     return m[len(items)][capacity]
 
+def frackspGreedy(items, capacity):
+    acc = 0
+    solution = 0
+    costBenefit = [int(items[i][0]/items[i][1]) for i in range(len(items))]
+    indexes, _ = zip(*sorted(enumerate(costBenefit), key=itemgetter(1)))
+    length = len(indexes)
+    for i in range(length):
+        acc += items[indexes[length - 1 -i]][1]
+        if acc <= capacity:
+            solution += items[indexes[length - 1 -i]][0]
+        else:
+            solution += items[indexes[length - 1 -i]][0] * (items[indexes[length - 1 -i]][1] - (acc-capacity))/items[indexes[length - 1 -i]][1]
+            return solution
+    return solution
+
 items = [[60, 10], [100, 20], [120, 30]]
 capacity = 50
-print("Bruteforce Exact Solution: " + str(kspbruteForce(items, capacity)))
+print("KSP: ")
+print("Bruteforce Solution: " + str(kspbruteForce(items, capacity)))
 print("Greedy Approximation algorithm: " + str(kspGreedy(items, capacity)))
-print("Dynamic Programming Exact Solution: " + str(kspDynProg(items, capacity)))
+print("Dynamic Programming Solution: " + str(kspDynProg(items, capacity)))
+
+print("\nFractional KSP: ")
+print("Greedy Solution: " + str(frackspGreedy(items, capacity)))
